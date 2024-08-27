22034e53a0f78ba6d9fcaa34457ee9a0a578a372
shaban14617 <a.shaban14617@gmail.com>
2024-08-27 15:34:51 +0300

    Adding digital print egypt

--- components/CardContainer.js
+++ components/CardContainer.js
@@ -1,3 +1,68 @@
+// 'use client';
+// import { useEffect } from 'react';
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
+// import Card from './ui/Card';
+
+// function CardContainer() {
+//   useEffect(() => {
+//     AOS.init({ duration: 1000, easing: 'ease-in-out' });
+//   }, []);
+
+//   return (
+//     <section className="py-36 px-4 bg-gray-100 min-h-screen" id="services">
+//       <div className="container mx-auto">
+//         <h1 className="text-4xl font-bold text-center mb-12">Solutions</h1>
+//         <div className="flex flex-wrap justify-center gap-8">
+//           <Card
+//             image={'/cardImages/DigitalMarketing.svg'}
+//             title="Digital Marketing"
+//             description="Elevate your brand with impactful, accessible messages that drive audience engagement and encourage meaningful action."
+//             points={[
+//               'Increase brand visibility.',
+//               'Drive customer engagement.',
+//               'Improve conversion rates.',
+//             ]}
+//           />
+//           <Card
+//             image={'/cardImages/CreativeDesign.webp'}
+//             title="Brand Identity"
+//             description="Brand identity design, tone-of-voice, and design systems that capture the essence of your company and culture."
+//             points={[
+//               "Define your brand's unique voice.",
+//               'Establish consistent branding.',
+//               'Create a memorable identity.',
+//             ]}
+//           />
+//           <Card
+//             image={'/cardImages/CODE.svg'}
+//             title="Software Development Services"
+//             description="Custom software solutions to drive your business forward. We deliver scalable, secure, and efficient applications and integrations tailored to your unique needs."
+//             points={[
+//               'Custom software development.',
+//               'Scalable and secure systems.',
+//               'Seamless integrations and automation.',
+//             ]}
+//           />
+//           <a href="http://digitalprintegypt.com/" target="_blank">
+//             <Card
+//               image={'/cardImages/DigitalPrining.svg'}
+//               title="Digital Print"
+//               description="we print large or very small high quality low cost with a quick reliable ‭& ‬honest service‭."
+//               points={[
+//                 'In&Ourdoor Printing',
+//                 'Digital Printing',
+//                 'MUGS, T-SHIRTS,BAGS & MORE',
+//               ]}
+//             />
+//           </a>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
+
+// export default CardContainer;
 'use client';
 import { useEffect } from 'react';
 import AOS from 'aos';
@@ -15,7 +80,7 @@ function CardContainer() {
         <h1 className="text-4xl font-bold text-center mb-12">Solutions</h1>
         <div className="flex flex-wrap justify-center gap-8">
           <Card
-            image={'/cardImages/Digital_Marketing.svg'}
+            image={'/cardImages/DigitalMarketing.svg'}
             title="Digital Marketing"
             description="Elevate your brand with impactful, accessible messages that drive audience engagement and encourage meaningful action."
             points={[
@@ -34,21 +99,6 @@ function CardContainer() {
               'Create a memorable identity.',
             ]}
           />
-          <a href="http://digitalprintegypt.com/" target="_blank">
-            <Card
-              image={'/cardImages/DigitalPrining.svg'}
-              title="digitalprintegypt.com"
-              description="WE PRINT EVERYTHING
-            We say that not because we're good at printing pretty much everything, but we've gathered years
-            of knowledge and can help you decide the best way to bring your work to life!
-            "
-              points={[
-                'In&Ourdoor Printing',
-                'Digital Printing',
-                'MUGS, T-SHIRTS,BAGS & MORE',
-              ]}
-            />
-          </a>
           <Card
             image={'/cardImages/CODE.svg'}
             title="Software Development Services"
@@ -58,6 +108,17 @@ function CardContainer() {
               'Scalable and secure systems.',
               'Seamless integrations and automation.',
             ]}
+          />
+          <Card
+            image={'/cardImages/DigitalPrining.svg'}
+            title="Digital Print"
+            description="We print large or very small high quality low cost with a quick reliable ‭& ‬honest service‭."
+            points={[
+              'In&Ourdoor Printing',
+              'Digital Printing',
+              'MUGS, T-SHIRTS, BAGS & MORE',
+            ]}
+            href="http://digitalprintegypt.com/"
           />
         </div>
       </div>
--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,13 +1,79 @@
+// 'use client';
+// import { useEffect } from 'react';
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
+// import Image from 'next/image';
+
+// function Card({ image, title, description, points }) {
+//   useEffect(() => {
+//     AOS.init({ duration: 1000, easing: 'ease-in-out' });
+//   }, []);
+
+//   return (
+//     <div
+//       className="w-full max-w-xs mx-auto bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform hover:scale-105 hover:shadow-2xl"
+//       data-aos="fade-up"
+//       style={{ height: '650px' }}
+//     >
+//       <div className="relative">
+//         <Image
+//           src={image}
+//           alt={title}
+//           width={400}
+//           height={300}
+//           className="object-cover w-full h-full"
+//         />
+//       </div>
+//       <div className="p-6 flex flex-col justify-between h-full">
+//         <div>
+//           <h2 className="text-2xl font-semibold mb-2 text-gray-800 text-center">
+//             {title}
+//           </h2>
+//           <p className="text-gray-600 mb-4 text-center">{description}</p>
+//         </div>
+//         <div className="overflow-hidden flex-grow">
+//           <div className="collapse collapse-arrow">
+//             <input type="checkbox" className="peer" />
+//             <div className="collapse-title text-lg font-medium text-center py-4">
+//               {' '}
+//               Learn More
+//             </div>
+//             <div
+//               className="collapse-content overflow-auto py-2"
+//               style={{ maxHeight: '200px' }}
+//             >
+//               <ul className="list-disc list-inside pb-2">
+//                 {points.map((point, index) => (
+//                   <li key={index} className="text-gray-700 mb-2">
+//                     {point}
+//                   </li>
+//                 ))}
+//               </ul>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Card;
 'use client';
 import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Image from 'next/image';
 
-function Card({ image, title, description, points }) {
+function Card({ image, title, description, points, href }) {
   useEffect(() => {
     AOS.init({ duration: 1000, easing: 'ease-in-out' });
   }, []);
+
+  const handleLearnMoreClick = () => {
+    if (href) {
+      window.open(href, '_blank');
+    }
+  };
 
   return (
     <div
@@ -32,24 +98,30 @@ function Card({ image, title, description, points }) {
           <p className="text-gray-600 mb-4 text-center">{description}</p>
         </div>
         <div className="overflow-hidden flex-grow">
-          <div className="collapse collapse-arrow">
+          <div
+            className="collapse collapse-arrow"
+            onClick={handleLearnMoreClick}
+            style={{ cursor: href ? 'pointer' : 'default' }}
+          >
             <input type="checkbox" className="peer" />
             <div className="collapse-title text-lg font-medium text-center py-4">
               {' '}
               Learn More
             </div>
-            <div
-              className="collapse-content overflow-auto py-2"
-              style={{ maxHeight: '200px' }}
-            >
-              <ul className="list-disc list-inside pb-2">
-                {points.map((point, index) => (
-                  <li key={index} className="text-gray-700 mb-2">
-                    {point}
-                  </li>
-                ))}
-              </ul>
-            </div>
+            {!href && (
+              <div
+                className="collapse-content overflow-auto py-2"
+                style={{ maxHeight: '200px' }}
+              >
+                <ul className="list-disc list-inside pb-2">
+                  {points.map((point, index) => (
+                    <li key={index} className="text-gray-700 mb-2">
+                      {point}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       </div>
