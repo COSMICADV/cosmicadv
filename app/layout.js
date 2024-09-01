1859ec573dc261597f74bdf60cd5cc8092754ca6
shaban14617 <a.shaban14617@gmail.com>
2024-09-01 14:08:48 +0300

    meta

--- app/layout.js
+++ app/layout.js
@@ -6,16 +6,15 @@ import Head from 'next/head';
 export const metadata = {
   title: 'COSMIC ADV Solutions',
   description: 'Experienced Company For Advertising Solutions',
+  verification: {
+    'google-site-verification': 'po4duazFj70cHD1Lw-3gVwbYVlNRjWw_VDJ-hTPDe_8',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta
-          name="google-site-verification"
-          content="po4duazFj70cHD1Lw-3gVwbYVlNRjWw_VDJ-hTPDe_8"
-        />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
       <body>
